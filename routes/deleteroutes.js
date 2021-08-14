45d91fcdd8fbb9499f0d2dfb16cd51abc5710a5d
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-08-14 23:23:35 +0100

    Improvement on the error handling

--- routes/deleteroutes.js
+++ routes/deleteroutes.js
@@ -19,7 +19,7 @@ deleteRouter.delete('/:username/packages/:parcelid', async (req, res) => {
   const parcelid = parseInt(req.params.parcelid);
   const parcelCon = [req.params.username, parcelid];
   try {
-    const deletedParcel = deletePackage(parcelCon);
+    const deletedParcel = await deletePackage(parcelCon);
     res.json(deletedParcel.rows[0]);
   } catch (error) {
     res.status(400).json({ message: error.message });
