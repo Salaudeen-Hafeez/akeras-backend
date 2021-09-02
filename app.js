db68d6c3a8845117a3a452dc9e3098771ae95a55
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-09-02 02:18:30 +0100

    Working on the POST request

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ import deleteRouter from './routes/deleteroutes';
 
 const app = express();
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
