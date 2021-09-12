a9227eab93846648c6c0f8d751223b7c5dce416a
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-09-12 10:30:20 +0100

    Working on the view engine

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // The middleware for the home page
