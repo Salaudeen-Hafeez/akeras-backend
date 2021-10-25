d6a05f7f5eb030aee30a5e29106bb6275189658b
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2021-10-25 12:43:32 +0100

    working on delete route and token check

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 import express from 'express';
-import cors from 'cors';
 import postRouter from './routes/postroutes';
 import getRouter from './routes/getroutes';
 import updateRouter from './routes/updateroutes';
 import deleteRouter from './routes/deleteroutes';
 
 const app = express();
-app.use(cors(cors));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
