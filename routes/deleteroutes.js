4d35d8ece0463ed4e225ba7874e69209ce3f4466
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-25 18:40:03 +0100

    Made return value of delete route an object

--- routes/deleteroutes.js
+++ routes/deleteroutes.js
@@ -30,6 +30,7 @@ deleteRouter.delete(
     const status = req.params.status;
     try {
       const deletedParcel = await deletePackage(parcelCon);
+      res.json(deletedParcel);
       if (deletedParcel.rowCount !== 0) {
         const packages = await client.query('SELECT * FROM packages');
         res.json({ packages: packages.rows, package: deletedParcel });
